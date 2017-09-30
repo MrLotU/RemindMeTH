9778186994c4f09757b1193a3af9058bbfd17e12
MrLotU <j.koopman@jarict.nl>
2017-09-30 10:38:15 +0200

    Added more things, started on data source

--- Remind me/Controller/RemindersViewController.swift
+++ Remind me/Controller/RemindersViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class RemindersViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let tableview = UITableView(frame: CGRect.zero, style: .grouped)
+        
+        tableview.register(ReminderTableViewCell.self, forCellReuseIdentifier: "ReminderCell")
+        tableview.backgroundColor = UIColor.white
+        
+        return tableview
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,21 +25,16 @@ class RemindersViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
-    */
-
 }
--- Remind me/Model/RemindersDataSource.swift
+++ Remind me/Model/RemindersDataSource.swift
@@ -7,7 +7,60 @@
 //
 
 import UIKit
+import Foundation
+import CoreData
 
 class RemindersDataSource: NSObject {
+    fileprivate let tableView: UITableView
+    fileprivate let managedObjectContext = CDController.sharedInstance.managedObjectContext
+    fileprivate let fetchedResultsController: FetchedResultController
+
+    init(fetchRequest: NSFetchRequest<NSFetchRequestResult>, tableView: UITableView) {
+        self.tableView = tableView
+        
+        self.fetchedResultsController = FetchedResultController(fetchRequest: fetchRequest, managedObjectContext: self.managedObjectContext, tableView: self.tableView)
+        
+        super.init()
+    }
     
+    func performFetch(withPredicate predicate: NSPredicate?) {
+        self.fetchedResultsController.performFetch(withPredicate: predicate)
+        tableView.reloadData()
+    }
 }
+
+//MARK: UITableViewDataSource
+
+extension RemindersDataSource: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = fetchedResultsController.sections?[section] else { return 0 }
+        return section.numberOfObjects
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath)
+        let reminder = fetchedResultsController.object(at: indexPath) as! Reminder
+        
+        //TODO: Setup
+        
+        return cell
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
